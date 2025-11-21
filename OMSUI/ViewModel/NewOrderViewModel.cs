379b074e7e09c38579b04edbe77ea5822f1f91f0
IlhamHashi <iaha71375@edu.ucl.dk>
2025-11-21 14:52:24 +0300

    updates to viewmodel

--- OMSUI/ViewModel/NewOrderViewModel.cs
+++ OMSUI/ViewModel/NewOrderViewModel.cs
@@ -1,46 +1,51 @@
-﻿using OrderManagerLibrary.Model.Classes;
+﻿using OrderManagerDesktopUI.ViewModel;
+using OrderManagerLibrary.Model.Classes;
 using OrderManagerLibrary.Model.Interfaces;
 using OrderManagerLibrary.Service;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 
 namespace OrderManagerLibrary.ViewModel;
-public class NewOrderViewModel
+public class NewOrderViewModel : ViewModelBase
 {
     private readonly OrderService _orderservice;
 
     private Product? selectedProduct;
-	private Order? newOrder;
 	private INote? orderNote;
-	private ICustomer? selectedCustomer;
-	private ICollectionType? collection;
+	private ICustomer selectedCustomer;
 	private IPaymentMethod? selectedPaymentMethod;
     private OrderStatus orderStatus;    
+    private DateTime collectionDateTime;
+    private string collectionNeighborhood;    
     private decimal paymentAmount;	
 	private int? selectedQuantity;
 	private bool isDelivery;    
 	private decimal? orderTotal;
-	private decimal outstandingAmount;	
+	private decimal outstandingAmount;
 
-	private List<IPaymentMethod>? _paymentMethods {  get; set; }
-	private List<Payment>? _payments { get; set; }
+
+    private List<IPaymentMethod>? _paymentMethods {  get; set; } = [];
+    private List<Payment>? _payments { get; set; } = [];
     private List<OrderLine> _orderLines { get; set; }
     public ObservableCollection<OrderLine>? OrderLines { get; set; }    
     public ObservableCollection<Product>? Products;
 
-    public ICommand CreateOrderCommand { get; set; }
-    public ICommand CancelOrderCommand { get; set; }
-    public ICommand ContinueToPaymentCommand { get; set; }
-    public ICommand GoBackToOrderDetailsCommand { get; set; }
-    public ICommand AddToOrderCommand { get; set; }
-    public ICommand SelectProductCommand { get; set; }
+    public ICommand CreateOrderCommand => new RelayCommand(execute => AddNewOrder(), canExecute => CanAddNewOrder());
+    public ICommand AddPaymentCommand => new RelayCommand(execute => AddPaymentToOrder(), canExecute => CanAddPaymentToOrder());
+    public ICommand CancelNewOrderCommand => new RelayCommand(execute => CancelNewOrder(), canExecute => CanCancelNewOrder());
+    public ICommand ContinueToPaymentCommand => new RelayCommand(execute => AddNewOrder(), canExecute => CanAddNewOrder());
+    public ICommand GoBackToOrderDetailsCommand => new RelayCommand(execute => AddNewOrder(), canExecute => CanAddNewOrder());
+    public ICommand AddToOrderCommand => new RelayCommand(execute => AddNewOrder(), canExecute => CanAddNewOrder());
+    public ICommand SelectProductCommand => new RelayCommand(execute => AddNewOrder(), canExecute => CanAddNewOrder());
 
-    private bool CanCreateOrder() => true; // Placeholder for actual logic
+    private bool CanAddNewOrder() => true; // Placeholder for actual logic
     private bool CanCancelOrder() => true; // Placeholder for actual logic
     private bool CanContinueToPayment() => true; // Placeholder for actual logic
     private bool CanGoBackToOrderDetails() => true; // Placeholder for actual logic
     private bool CanAddToOrder() => true; // Placeholder for actual logic
     private bool CanSelectProduct() => true; // Placeholder for actual logic
+    private bool CanAddPaymentToOrder() => true;
 
 
     public Product? SelectedProduct
@@ -48,25 +53,15 @@ public class NewOrderViewModel
         get { return selectedProduct; }
         set { selectedProduct = value; }
     }
-    public Order? NewOrder
-    {
-        get { return newOrder; }
-        set { newOrder = value; }
-    }
     public INote? OrderNote
     {
         get { return orderNote; }
         set { orderNote = value; }
     }
-     public ICustomer? SelectedCustomer
+     public ICustomer SelectedCustomer
     {
         get { return selectedCustomer; }
         set { selectedCustomer = value; }
-    }
-	public ICollectionType? CollectionType
-    {
-        get { return collection; }
-        set { collection = value; }
     }
 	public IPaymentMethod? SelectedPaymentMethod
 	{
@@ -78,7 +73,16 @@ public class NewOrderViewModel
         get { return orderStatus; }
         set { orderStatus = value; }
     }
-
+    public DateTime CollectionDateTime
+    {
+        get { return collectionDateTime; }
+        set { collectionDateTime = value; }
+    }
+    public string CollectionNeighborhood
+    {
+        get { return collectionNeighborhood; }
+        set { collectionNeighborhood = value; }
+    }
     public decimal PaymentAmount
 	{
 		get { return paymentAmount; }
@@ -112,7 +116,45 @@ public class NewOrderViewModel
 
     private void AddNewOrder()
     {
-        _orderservice.CreateOrder(NewOrder, _orderLines, _paymentMethods, _payments, CollectionType, OrderNote);
+        Order newOrder = new(DateTime.Now, OrderStatus.Draft, selectedCustomer.CustomerId);
+        ICollectionType collection = HandleCollectionType();
+        _orderservice.CreateOrder(newOrder, _orderLines, _paymentMethods, _payments, collection, OrderNote);
+
+        MessageBox.Show($"Order {newOrder.OrderId} has been saved succesfully");
     }
+
+    private void AddProductToOrder()
+    {
+        // opretter et orderline instans
+        new orderline
+        // opdaterer _orderlines så den inkluderer alle selectedproducts
+        _orderlines.add()
+    }
+
+    private void AddPaymentToOrder()
+    {
+        //
+
+    }
+
+    private ICollectionType HandleCollectionType()
+    {
+        ICollectionType delivery;
+        ICollectionType pickUp;
+        if (IsDelivery) 
+        {
+            delivery = new Delivery (DateTime.Now, CollectionNeighborhood);
+            return delivery;
+        }
+        else
+        {
+            pickUp = new PickUp (DateTime.Now);
+            return pickUp;
+        }
+    }
+
+
+
+
 
 }
